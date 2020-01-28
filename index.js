b1cd01621c951278e22581af3b75e9171e2fbde6
Anna Dybas <a.m.dybas@gmail.com>
2020-01-28 12:58:56 -0500

    Done.

--- index.js
+++ index.js
@@ -12,23 +12,23 @@ const tutorials = [
 ];
 
 
-// const titleCased = () => {
-//   return tutorials.map(tutorial => {
-//     debugger
-//     let words = tutorial.split(' ');
-//     words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
-//   });
-// };
+const titleCased = () => {
+  return tutorials.map(tutorial => {
+    let words = tutorial.split(' ');
+    let capWord = words.map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    let capSentence = capWord.join(" ")
+    return capSentence
+  })
+};
 
 // // return tutorials.map(tutorial => function(){
-// //   tutorial.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(0));
-// // }).join(" ");
+// //   tutorial.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(0)).join(" ");
 
-function toTitleCase(string){
-  let words = string.split(' ');
-  let capitalizeWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
-  return capitalizeWords;
-}
-const titleCased = () => {
-  return tutorials.map(tutorial => toTitleCase(tutorial));
-}
+// function toTitleCase(string){
+//   let words = string.split(' ');
+//   let capitalizeWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+//   return capitalizeWords;
+// }
+// const titleCased = () => {
+//   return tutorials.map(tutorial => toTitleCase(tutorial));
+// }
