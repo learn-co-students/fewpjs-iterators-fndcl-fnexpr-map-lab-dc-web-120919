5743a06b67447b8b2b458f57fb830ad82343496a
John Louis Rumingan <jmrumingan@gmail.com>
2020-01-27 19:46:08 -0500

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,27 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+
+const titleCased = function(input){
+  //maps tutorials array 
+  let outputArray= tutorials.map(tutorial => {
+
+    // makes new array of a single title 
+    let title = tutorial.split(' ');
+
+    //makes new array of the words in the single title
+    //changes each word to uppercase 
+    let capitalTitle = title.map(word =>  word.charAt(0).toUpperCase() + word.slice(1));
+    //joins all the words again, now uppercase 
+    let titleJoin = capitalTitle.join(' ');
+    
+    //returns this newly-formed title to map to original tutorials array
+    return titleJoin;
+  });
+
+  //returns the newly formed array 
+  return outputArray;
 }
+
+let finalTitles = titleCased(tutorials);
+console.log(finalTitles);
